d4f041fc31981cc15feb667e579bcb8ead298921
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-04 21:09:32 +0200

    [dev] (feat): add createWorkspace mutation

--- build/dbSetup.sql
+++ build/dbSetup.sql
@@ -1,10 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS sc_work;
 
 DROP TABLE IF EXISTS sc_work.workspace;
-CREATE TABLE sc_work.workspace();
+CREATE TABLE sc_work.workspace(
+  name      varchar(70)   PRIMARY KEY
+);
 
 DROP USER IF EXISTS service_work;
 CREATE USER service_work WITH PASSWORD xxxxx;
 
 GRANT USAGE ON SCHEMA sc_work TO service_work;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA sc_wor TO service_work;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA sc_work TO service_work;
--- src/rootResolvers.js
+++ src/rootResolvers.js
@@ -7,11 +7,21 @@ const knex = require('knex')({
   connection: {
     host: process.env.DATABASE_URL,
     database: process.env.DATABASE_NAME,
-    user: 'service_workspace',
+    user: 'service_work',
     password: process.env.WORKSPACE_DATABASE_PASSWORD
   }
 })
+const dbSchema = 'sc_work'
+const select = async (from, where) => await knex.select().withSchema(dbSchema).from(from).where(where)
+const selectSingle = async (from, where) => await select(from, where) |> (_ => #.length ?#[0] : null) ()
 
 export default {
-  hello: () => 'hello there!'
+  hello: () => 'hello there!',
+
+  createWorkspace: async({name}) => {
+    const exists = await selectSingle('workspace', {name}) ? true : false
+    if (exists)
+      throw new Error('ALREADY_EXISTS')
+    await knex.withSchema(dbSchema).into('workspace').insert({name})
+  }
 }
