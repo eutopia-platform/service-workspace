653ebc8dc3970a3338bb6f52fd9ee279d4fd73a8
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-16 16:22:34 +0200

    [CUBE-17] (feat): implement workspace query

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,3 +1,7 @@
+import { AuthenticationError, UserInputError, gql } from 'apollo-server-micro'
+import crypto from 'crypto'
+import { user as userService } from './interService'
+
 const knex = require('knex')({
   client: 'pg',
   version: '10.6',
@@ -12,6 +16,49 @@ const knex = require('knex')({
 
 export default {
   Query: {
-    hello: () => 'workspace service says hello'
+    hello: () => 'workspace service says hello',
+
+    workspace: async (root, { name }, context) => {
+      if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
+
+      let workspace = (await knex('workspace').where({ name }))[0]
+      if (!workspace)
+        workspace = (await knex('workspace').select()).find(
+          e => e.name.toLowerCase() === name.toLowerCase()
+        )
+      if (!workspace)
+        throw new UserInputError(`workspace with name "${name}" doesn't exist`)
+
+      return workspace
+    }
+  },
+
+  Workspace: {
+    created: ({ created }) => (created ? created.toISOString() : null),
+    members: async ({ uid }) => {
+      return (await userService.query({
+        query: gql`
+          query getUsers($ids: [ID!]!) {
+            usersById(ids: $ids) {
+              id
+              name
+              callname
+              email
+            }
+          }
+        `,
+        variables: {
+          ids: await knex(`${uid}_member`).map(user => user.uid)
+        }
+      })).data.usersById
+    }
+  },
+
+  User: {
+    id: user =>
+      crypto
+        .createHash('sha256')
+        .update(user.id)
+        .digest('base64')
   }
 }
