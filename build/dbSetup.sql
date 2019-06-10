3f68a26aac6eae43e7147d75f1429b5f364f05a9
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-10 18:00:02 +0200

    [dev] (feat): new database schema

--- build/dbSetup.sql
+++ build/dbSetup.sql
@@ -1,23 +1,17 @@
-CREATE SCHEMA IF NOT EXISTS sc_work;
+CREATE SCHEMA IF NOT EXISTS schema_workspace;
 
-DROP TABLE IF EXISTS sc_work.workspace;
-CREATE TABLE sc_work.workspace(
-  uid       text          PRIMARY KEY,
-  name      varchar(70)   UNIQUE NOT NULL,
+DROP TABLE IF EXISTS schema_workspace.workspace;
+CREATE TABLE schema_workspace.workspace(
+  id        uuid          PRIMARY KEY,
+  name      varchar       UNIQUE NOT NULL,
+  members   uuid[]        NOT NULL,
+  invited   uuid[]        NOT NULL,
   created   timestamptz
-);
-
-DROP TABLE IF EXISTS sc_work.invitation;
-CREATE TABLE sc_work.invitation(
-  link            text          PRIMARY KEY,
-  workspace       text          NOT NULL,
-  invitee         char(20)      NOT NULL,
-  issuer          char(20),
-  created         timestamptz
 );
 
 DROP USER IF EXISTS service_work;
 CREATE USER service_work WITH PASSWORD xxxxx;
 
-GRANT CREATE, USAGE ON SCHEMA sc_work TO service_work;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA sc_work TO service_work;
+GRANT CREATE, USAGE ON SCHEMA schema_workspace TO service_work;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA schema_workspace TO service_work;
+ALTER DEFAULT PRIVILEGES IN SCHEMA schema_workspace GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO eutopia;
--- src/resolvers.js
+++ src/resolvers.js
@@ -9,6 +9,7 @@ import crypto from 'crypto'
 import { user as userService } from './interService'
 import randomString from './randomString'
 import { isValidEmail, sendInvitation as sendInviteEmail } from './mail'
+import uuid from 'uuid/v4'
 
 const knex = require('knex')({
   client: 'pg',
@@ -19,57 +20,50 @@ const knex = require('knex')({
     user: 'service_work',
     password: process.env.WORKSPACE_DATABASE_PASSWORD
   },
-  searchPath: 'sc_work'
+  searchPath: 'schema_workspace'
 })
 
-let validWorkspaceName = name =>
+const validWorkspaceName = name =>
   name.length >= 3 &&
   /^[a-z0-9_-]+$/i.test(name) &&
   !/^[_-]|[_-]$/.test(name) &&
   !/([_-])\1+/.test(name)
 
+const usersById = async ids =>
+  (await userService.query({
+    query: gql`
+      query workspaceMembers($ids: [ID!]!) {
+        usersById(ids: $ids) {
+          id
+          name
+          callname
+          email
+        }
+      }
+    `,
+    variables: {
+      ids
+    }
+  })).data.usersById
+
 export default {
   Query: {
     hello: () => 'workspace service says hello',
 
-    workspace: async (root, { name }, context) => {
-      if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
-
-      let workspace = (await knex('workspace').where({ name }))[0]
-      if (!workspace)
-        workspace = (await knex('workspace').select()).find(
-          e => e.name.toLowerCase() === name.toLowerCase()
-        )
-      if (!workspace)
-        throw new UserInputError(`workspace with name "${name}" doesn't exist`)
-
-      return workspace
+    workspace: async (root, { name }, { userId }) => {
+      if (!userId) throw new AuthenticationError('NOT_LOGGED_IN')
+      const space = (await knex('workspace').where({ name }))[0]
+      if (!space) throw new UserInputError('DOES_NOT_EXIST')
+      if (!space.members.includes(userId))
+        throw new ForbiddenError('NOT_MEMBER')
+      return space
     },
 
-    workspaces: async (root, args, context) => {
-      if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
-      const spaceIds = await knex('workspace')
-        .select('uid')
-        .map(s => s.uid)
-      const memberSpaces = (await Promise.all(
-        spaceIds.map(async id => await knex(id + '_member').map(m => m.uid))
-      ))
-        .map(m => m.includes(context.userId))
-        .map((v, i) => [spaceIds[i], v])
-        .reduce((acc, c) => (c[1] ? acc.concat(c[0]) : acc), [])
-
-      return await knex('workspace').whereIn('uid', memberSpaces)
-    },
-
-    inviteSpaceName: async (root, { link }, context) => {
-      const invite = (await knex('invitation').where({ link }))[0]
-      if (!invite) throw new ForbiddenError()
-      if (context.userId !== invite.invitee) throw new ForbiddenError()
-      const space = (await knex('workspace').where({
-        uid: invite.workspace
-      }))[0]
-      if (!space) throw new ApolloError('WORKSPACE_GONE')
-      return space.name
+    workspaces: async (root, args, { userId }) => {
+      if (!userId) throw new AuthenticationError('NOT_LOGGED_IN')
+      return (await knex('workspace').select()).filter(space =>
+        space.members.includes(userId)
+      )
     }
   },
 
@@ -77,175 +71,139 @@ export default {
     createWorkspace: async (root, { name }, context) => {
       if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
       if (!validWorkspaceName(name)) throw new UserInputError('INVALID_NAME')
-      const exists = (await knex('workspace').select('name'))
-        .map(s => s.name.toLowerCase())
-        .includes(name.toLowerCase())
-      if (exists) throw new UserInputError('ALREADY_EXISTS')
-      let uid = randomString(8, { lower: true })
-      while ((await knex('workspace').where({ uid }).length) > 0)
-        uid = randomString(8, { lower: true })
-      const space = (await knex
-        .into('workspace')
-        .insert({ uid, name, created: new Date().toISOString() })
-        .returning('*'))[0]
-      await knex.schema.createTable(`${uid}_member`, table => {
-        table.string('uid').primary()
-      })
-      await knex.into(`${uid}_member`).insert({ uid: context.userId })
-      return space
-    },
-
-    invite: async (root, { workspace, email }, context) => {
-      if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
-      const space = (await knex('workspace').where({ name: workspace }))[0]
-      if (!space) throw new ForbiddenError()
-
-      const memberUids = await knex(space.uid + '_member').map(m => m.uid)
-      const isMember = memberUids.includes(context.userId)
-      if (!isMember) throw new ForbiddenError()
-
-      if (!isValidEmail(email)) throw new UserInputError('INVALID_EMAIL')
-
-      let invitee = (await userService.query({
-        query: gql`
-          query getAccount($email: String!) {
-            usersByEmail(emails: [$email]) {
-              id
-            }
-          }
-        `,
-        variables: {
-          email
-        }
-      })).data.usersByEmail[0]
-
-      console.log('invitee:', invitee)
-
-      if (!invitee) throw new UserInputError('NOT_USER')
-      invitee = invitee.id
-
-      if (memberUids.includes(invitee))
-        throw new UserInputError('ALREADY_MEMBER')
-
-      const alreadInvited =
-        (await knex('invitation')
-          .select()
-          .where({ workspace, invitee })).length > 0
-
-      if (alreadInvited) throw new UserInputError('ALREADY_INVITED')
-
-      let link
-      do {
-        link = randomString(6, { lower: true, number: true })
-      } while ((await knex('invitation').where({ link })).length > 0)
-
-      await knex('invitation').insert({
-        link: link,
-        workspace: space.uid,
-        invitee: invitee,
-        issuer: context.userId,
-        created: new Date().toISOString()
-      })
-
-      const [inviteeName, userName] = (await userService.query({
-        query: gql`
-          query inviteeName($uidInvitee: ID!, $uidUser: ID!) {
-            usersById(ids: [$uidInvitee, $uidUser]) {
-              callname
-            }
-          }
-        `,
-        variables: {
-          uidInvitee: invitee,
-          uidUser: context.userId
-        }
-      })).data.usersById.map(user => user.callname)
-
-      await sendInviteEmail(
-        email,
-        space.name,
-        userName,
-        inviteeName,
-        `https://productcube.io/invite/${link}`
+      if (
+        (await knex('workspace').select('name'))
+          .map(s => s.name.toLowerCase())
+          .includes(name.toLowerCase())
       )
+        throw new UserInputError('ALREADY_EXISTS')
+
+      return (await knex('workspace')
+        .insert({
+          id: uuid(),
+          name,
+          members: [context.userId],
+          invited: [],
+          created: knex.fn.now()
+        })
+        .returning('*'))[0]
     },
 
-    joinWorkspace: async (root, { inviteLink }, context) => {
-      if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
-      const invite = (await knex('invitation').where({ link: inviteLink }))[0]
-      if (!invite) throw new ForbiddenError()
-      if (invite.invitee !== context.userId) throw new ForbiddenError()
+    // invite: async (root, { workspace, email }, context) => {
+    //   if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
+    //   const space = (await knex('workspace').where({ name: workspace }))[0]
+    //   if (!space) throw new ForbiddenError()
 
-      const space = (await knex('workspace').where({
-        uid: invite.workspace
-      }))[0]
-      if (!space) throw new ApolloError('WORKSPACE_GONE')
+    //   const memberUids = await knex(space.uid + '_member').map(m => m.uid)
+    //   const isMember = memberUids.includes(context.userId)
+    //   if (!isMember) throw new ForbiddenError()
 
-      await knex(`${space.uid}_member`).insert({ uid: invite.invitee })
-      await knex('invitation')
-        .where({ invitee: invite.invitee })
-        .del()
+    //   if (!isValidEmail(email)) throw new UserInputError('INVALID_EMAIL')
 
-      return (await knex('workspace').where({ uid: space.uid }))[0]
-    },
+    //   let invitee = (await userService.query({
+    //     query: gql`
+    //       query getAccount($email: String!) {
+    //         usersByEmail(emails: [$email]) {
+    //           id
+    //         }
+    //       }
+    //     `,
+    //     variables: {
+    //       email
+    //     }
+    //   })).data.usersByEmail[0]
 
-    deleteWorkspace: async (root, { name }, context) => {
-      if (!context.userId) throw new ForbiddenError()
+    //   console.log('invitee:', invitee)
+
+    //   if (!invitee) throw new UserInputError('NOT_USER')
+    //   invitee = invitee.id
+
+    //   if (memberUids.includes(invitee))
+    //     throw new UserInputError('ALREADY_MEMBER')
+
+    //   const alreadInvited =
+    //     (await knex('invitation')
+    //       .select()
+    //       .where({ workspace, invitee })).length > 0
+
+    //   if (alreadInvited) throw new UserInputError('ALREADY_INVITED')
+
+    //   let link
+    //   do {
+    //     link = randomString(6, { lower: true, number: true })
+    //   } while ((await knex('invitation').where({ link })).length > 0)
+
+    //   await knex('invitation').insert({
+    //     link: link,
+    //     workspace: space.uid,
+    //     invitee: invitee,
+    //     issuer: context.userId,
+    //     created: new Date().toISOString()
+    //   })
+
+    //   const [inviteeName, userName] = (await userService.query({
+    //     query: gql`
+    //       query inviteeName($uidInvitee: ID!, $uidUser: ID!) {
+    //         usersById(ids: [$uidInvitee, $uidUser]) {
+    //           callname
+    //         }
+    //       }
+    //     `,
+    //     variables: {
+    //       uidInvitee: invitee,
+    //       uidUser: context.userId
+    //     }
+    //   })).data.usersById.map(user => user.callname)
+
+    //   await sendInviteEmail(
+    //     email,
+    //     space.name,
+    //     userName,
+    //     inviteeName,
+    //     `https://productcube.io/invite/${link}`
+    //   )
+    // },
+
+    // joinWorkspace: async (root, { inviteLink }, context) => {
+    //   return
+    //   if (!context.userId) throw new AuthenticationError('NOT_LOGGED_IN')
+    //   const invite = (await knex('invitation').where({ link: inviteLink }))[0]
+    //   if (!invite) throw new ForbiddenError()
+    //   if (invite.invitee !== context.userId) throw new ForbiddenError()
+
+    //   const space = (await knex('workspace').where({
+    //     uid: invite.workspace
+    //   }))[0]
+    //   if (!space) throw new ApolloError('WORKSPACE_GONE')
+
+    //   await knex(`${space.uid}_member`).insert({ uid: invite.invitee })
+    //   await knex('invitation')
+    //     .where({ invitee: invite.invitee })
+    //     .del()
+
+    //   return (await knex('workspace').where({ uid: space.uid }))[0]
+    // },
+
+    deleteWorkspace: async (root, { name }, { userId }) => {
+      if (!userId) throw new ForbiddenError()
       const space = (await knex('workspace').where({ name }))[0]
       if (!space) throw new ForbiddenError()
-      const isMember =
-        (await knex(`${space.uid}_member`).where({ uid: context.userId }))
-          .length > 0
-      if (!isMember) throw new ForbiddenError()
-
-      await knex('invitation')
-        .where({ workspace: space.uid })
-        .del()
-      await knex.schema.dropTable(`${space.uid}_member`)
+      if (
+        !(await knex('workspace')
+          .select('members')
+          .where({ name }))[0].members.includes(userId)
+      )
+        throw new ForbiddenError()
       await knex('workspace')
-        .where({ uid: space.uid })
+        .where({ id: space.id })
         .del()
     }
   },
 
   Workspace: {
     created: ({ created }) => (created ? created.toISOString() : null),
-    members: async ({ uid }) =>
-      (await userService.query({
-        query: gql`
-          query getUsers($ids: [ID!]!) {
-            usersById(ids: $ids) {
-              id
-              name
-              callname
-              email
-            }
-          }
-        `,
-        variables: {
-          ids: await knex(`${uid}_member`).map(user => user.uid)
-        }
-      })).data.usersById,
-
-    invited: async ({ uid }) =>
-      (await userService.query({
-        query: gql`
-          query getUsers($ids: [ID!]!) {
-            usersById(ids: $ids) {
-              id
-              name
-              callname
-              email
-            }
-          }
-        `,
-        variables: {
-          ids: await knex('invitation')
-            .select('invitee')
-            .where({ workspace: uid })
-            .map(e => e.invitee)
-        }
-      })).data.usersById
+    members: async ({ members }) => await usersById(members),
+    invited: async ({ invited }) => await usersById(invited)
   },
 
   User: {
